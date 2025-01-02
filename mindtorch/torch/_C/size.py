6d38a2b8e9b9e76f73c27ef85031538077ef95a0
lvyufeng <lvyufeng@cqu.edu.cn>
2025-01-02 11:30:27 +0800

    fix torch.Size

--- mindtorch/torch/_C/size.py
+++ mindtorch/torch/_C/size.py
@@ -3,11 +3,13 @@ from functools import reduce
 
 
 def _get_tuple_numel(input):
+    if input == ():
+        return 1
     return reduce(operator.mul, list(input))
 
 
 class Size(tuple):
-    def __new__(cls, shape):
+    def __new__(cls, shape=()):
         _shape = shape
         if not isinstance(_shape, (tuple, list)):
             raise TypeError("{} object is not supportted.".format(type(shape)))
